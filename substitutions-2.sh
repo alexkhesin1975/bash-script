29ca4b43ffbf2cec8a879fa8174b840509e3f1e1
Alex <alex_khesin@hotmail.com>
2019-03-04 01:54:49 +0200

    second example of substitution with command output to interprettor

---
+++ substitutions-2.sh
@@ -0,0 +1,6 @@
+# Shell substitutions
+# Command substitution example
+
+today_date=`date` #if we want ti insert output of some command to interprettor the command shoud be between apostrofs [ ` == on the ~ button ] [ `date` ]
+
+echo $today_date " seems to be a lovely day!"
